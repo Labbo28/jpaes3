f71baec30f69bcc299c804290c83637632c313a2
Labbo28 <labonia.riccardo@gmail.com>
2025-03-27 10:02:52 +0100

    first commit

---
+++ src/main/java/it/uniroma3/siw/jpaes3/jpaes3/Jpaes3Application.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.jpaes3.jpaes3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpaes3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpaes3Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/jpaes3/jpaes3/Jpaes3ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.jpaes3.jpaes3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpaes3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
